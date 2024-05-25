f94022c37d7edaa77659d8345085dbfc66d4f058
juliofl82 <jfigueiredolamounier@gmail.com>
2024-05-24 23:28:55 -0300

    update profile page scss

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import TechList from '../TechList/TechList.jsx';
 import CreateTechModal from '../CreateTechModal/CreateTechModal.jsx';
 import useStore from '../../store/TechStore';
+import styles from "./styles.module.scss";
 
 const ProfilePage = () => {
   const { user, fetchUserProfile, logout, isModalOpen, openModal, closeModal } = useStore();
@@ -28,12 +29,20 @@ const ProfilePage = () => {
 
   return (
     <div>
-      <h1>Olá, {user.name}</h1>
-      <p>Módulo atual cadastrado para o usuário: {user.course_module}</p>
-      <button onClick={handleLogout}>Sair</button>
+       <div className={styles.profileHeader}>
+        <h1>Kenzie Hub</h1>
+        <button className='buttonDisabledHover' onClick={() => window.location.href = '/login'}>
+          Sair
+        </button>
+        </div>
+        <div className='profileGrid' >
+          <h2>Olá, {user.name}</h2>
+      <p>Módulo atual cadastrado para o usuário: {user.course_module}</p>      
       <button onClick={openModal}>Adicionar Tecnologia</button>
       <TechList onEdit={() => {}} />
       {isModalOpen && <CreateTechModal closeModal={closeModal} />}
+        </div>
+      
     </div>
   );
 };
